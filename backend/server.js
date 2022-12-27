eb57d812cbedf6ce4f5099f0f3b8101fc166b869
Allen Saji <allensaji@gmail.com>
2022-12-27 14:25:06 +0530

    set up mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+require('colors')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 const app = express()
 app.listen(port, () => console.log(`Server started on port ${port}`))
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
