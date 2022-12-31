34229924e47654808fae4dd472319018c7aee056
Allen Saji <allensaji@gmail.com>
2022-12-31 22:35:55 +0530

    add header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import { Route, Routes } from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login"
 import Signup from "./pages/Signup";
+import 'react-toastify/dist/ReactToastify.css'
+
 
 function App() {
   return (
     <>
-   
+    <div className="container">
+      <Header />
      <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/login" element={<Login />}/>
       <Route path="/signup" element={<Signup />}/>
      </Routes>
+     <Footer />
+    </div>
     </>
   )
 }
