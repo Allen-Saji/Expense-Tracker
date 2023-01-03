5cb5f86fb461b15c2038220275bee5c643bf75f4
Allen Saji <allensaji@gmail.com>
2023-01-03 07:31:24 +0530

    set up login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,25 +1,79 @@
-import React from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 import { Link } from 'react-router-dom'
 
-function login() {
-    $(".input_text").focus(function(){
-        $(this).prev('.fa').addclass('glowIcon')
-    })
-    $(".input_text").focusout(function(){
-        $(this).prev('.fa').removeclass('glowIcon')
-    })
+function Login() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { isLoading } = useSelector((state) => state.auth)
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  // NOTE: no need for useEffect here as we can catch the
+  // AsyncThunkAction rejection in our onSubmit or redirect them on the
+  // resolution
+  // Side effects shoulld go in event handlers where possible
+  // source: - https://beta.reactjs.org/learn/keeping-components-pure#where-you-can-cause-side-effects
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+      .unwrap()
+      .then((user) => {
+        // NOTE: by unwrapping the AsyncThunkAction we can navigate the user after
+        // getting a good response from our API or catch the AsyncThunkAction
+        // rejection to show an error message
+        toast.success(`Logged in as ${user.name}`)
+        navigate('/')
+      })
+      .catch(toast.error)
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+   
   return (
     <>
-         <div className="login_form_container">
+         <div className="login_form_container" id='login_form'>
+         <form onSubmit={onSubmit}>
       <div className="login_form">
         <h2>Login</h2>
         <div className="input_group">
           <i className="fa fa-user"></i>
           <input
             type="text"
-            placeholder="Username"
+            placeholder="Email"
             className="input_text"
             autoComplete="off"
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
           />
         </div>
         <div className="input_group">
@@ -29,18 +83,23 @@ function login() {
             placeholder="Password"
             className="input_text"
             autoComplete="off"
+            required
+            name='password'
+            value={password}
+            onChange={onChange}
           />
         </div>
         <div className="button_group" id="login_button">
-          <a>Submit</a>
+        <button>Submit</button>
         </div>
         <div className="footer">
           <Link to={"/signup"}>New User?</Link>
         </div>
       </div>
+      </form>
     </div>
     </>
   )
 }
 
-export default login
+export default Login
