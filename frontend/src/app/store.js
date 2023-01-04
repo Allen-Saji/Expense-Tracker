e5fcce13b6d9e7f554a9f3f44e716d9fdecfa09c
Allen Saji <allensaji@gmail.com>
2023-01-04 22:12:17 +0530

    add all transaction reducers

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+import creditReducer from '../features/transactions/creditSlice'
+import debitReducer from '../features/transactions/debitSlice'
+import transactionReducer from '../features/transactions/transactionSlice'
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    credit: creditReducer,
+    debit: debitReducer,
+    transaction: transactionReducer
   },
 })
